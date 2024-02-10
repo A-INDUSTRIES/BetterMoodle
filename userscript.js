287d835f37cb7a3bba82e9291879d50a6c51d0ce
AINDUSTRIES <contact.aindustries@gmail.com>
2024-02-10 13:51:01 +0100

    🔄️ v5 a great one

--- userscript.js
+++ userscript.js
@@ -11,6 +11,7 @@
 
 (function() {
     'use strict';
+    login();
     let ordering = getOrdering();
     parseStyle(ordering);
     addUi(ordering);
@@ -50,8 +51,23 @@ function addStyleSheet() {
     }
     
     .search {
-        color:red;
-        margin-bottom: 4px;;
+        color:black;
+        margin-bottom: 4px;
+        border: solid 1px black;
+        border-radius: 3px;
+    }
+    
+    .searchedempty {
+        border: None;
+        margin-bottom:0px;
+        padding: 0px 0px 0px 0px;
+    }
+    
+    .searchedfound {
+        border: solid 4px green;
+        border-radius: 5px;
+        margin-bottom: 4px;
+        padding: 2px 2px 2px 2px;
     }`;
     let head = document.getElementsByTagName("head")[0];
     head.appendChild(stylesheet);
@@ -59,44 +75,50 @@ function addStyleSheet() {
 
 function addUi() {
     let courses = document.querySelector(".courses");
-    const div = document.getElementById("carousel-item-main");
+    if (courses !== null) {
+        const div = document.getElementById("carousel-item-main");
 
-    let save = document.createElement("button");
-    save.textContent = "BetterMoodle Save Config";
-    save.addEventListener("click", saveOrdering);
-    save.setAttribute("class", "dropdown-item");
-
-    let del = document.createElement("button");
-    del.textContent = "BetterMoodle Delete Config";
-    del.addEventListener("click", deleteOrdering);
-    del.setAttribute("class", "dropdown-item");
-
-    div.insertBefore(save, div.getElementsByClassName("dropdown-divider")[1]);
-    div.insertBefore(del, div.getElementsByClassName("dropdown-divider")[1]);
-
-    courses.getElementsByClassName("coursebox").forEach(element => {
-        let index = parseInt(element.getAttribute("style").split(":")[1].replace(";", ""));
-        let input = document.createElement("input");
-        let box = document.createElement("div");
-        box.setAttribute("class", "box")
-        input.setAttribute("type", "number");
-        input.setAttribute("class", "input");
-        input.value = index;
-        box.appendChild(input);
-        element.appendChild(box);
-        input.addEventListener("change", () => {
+        let save = document.createElement("button");
+        save.textContent = "BetterMoodle Save Config";
+        save.addEventListener("click", saveOrdering);
+        save.setAttribute("class", "dropdown-item");
+    
+        let del = document.createElement("button");
+        del.textContent = "BetterMoodle Delete Config";
+        del.addEventListener("click", deleteOrdering);
+        del.setAttribute("class", "dropdown-item");
+    
+        div.insertBefore(save, div.getElementsByClassName("dropdown-divider")[1]);
+        div.insertBefore(del, div.getElementsByClassName("dropdown-divider")[1]);
+    
+        courses.getElementsByClassName("coursebox").forEach(element => {
             let index = parseInt(element.getAttribute("style").split(":")[1].replace(";", ""));
-            moveCourse(index, input.value);
+            let input = document.createElement("input");
+            let box = document.createElement("div");
+            box.setAttribute("class", "box")
+            input.setAttribute("type", "number");
+            input.setAttribute("class", "input");
+            input.value = index;
+            box.appendChild(input);
+            element.appendChild(box);
+            input.addEventListener("change", () => {
+                let index = parseInt(element.getAttribute("style").split(":")[1].replace(";", ""));
+                moveCourse(index, input.value);
+            });
         });
-    });
-
-    let list = document.getElementById("frontpage-course-list");
-    let search = document.createElement("input");
-    search.setAttribute("type", "text");
-    search.setAttribute("class", "search");
-    search.setAttribute("placeholder", "Recherhez un cours.");
-    search.addEventListener("input", () => {searchCourse(search.value)});
-    list.insertBefore(search, courses);
+    
+        let list = document.getElementById("frontpage-course-list");
+        let search = document.createElement("input");
+        search.setAttribute("type", "text");
+        search.setAttribute("class", "search");
+        search.setAttribute("placeholder", "Recherchez un cours.");
+        search.addEventListener("input", () => {searchCourse(search.value)});
+        list.insertBefore(search, courses);
+    
+        let searched = document.createElement("div");
+        searched.setAttribute("id", "searched");
+        list.insertBefore(searched, courses);
+    }  
 }
 
 function currentOrdering() {
@@ -127,6 +149,12 @@ function getCourseByOrder(index) {
     return courses[0];
 }
 
+function login() {
+    if (document.getElementsByClassName("login").length !== 0) {
+        document.location.replace("https://moodle.umons.ac.be/login/index.php");
+    }
+}
+
 function moveCourse(currentIndex, afterIndex) {
     let activeCourse = getCourseByOrder(currentIndex);
     let swappedCourse = getCourseByOrder(afterIndex);
@@ -138,21 +166,23 @@ function moveCourse(currentIndex, afterIndex) {
 
 function parseStyle(ordering={}) {
     let courses = document.querySelector(".courses");
-    let items = courses.getElementsByClassName("coursebox");
-    if (Object.keys(ordering).length !== 0) {
-        console.log("Parse with save");
-        items.forEach(element => {
-            let courseName = element.getElementsByClassName("info")[0].innerText;
-            element.setAttribute("style", `${ordering[courseName]}`);
-        });
-    } else {
-        console.log("Parse default");
-        for (let i=0; i<items.length; i++) {
-            items[i].setAttribute("style", `order:${i+1};`);
+    if (courses !== null) {
+        let items = courses.getElementsByClassName("coursebox");
+        if (Object.keys(ordering).length !== 0) {
+            console.log("Parse with save");
+            items.forEach(element => {
+                let courseName = element.getElementsByClassName("info")[0].innerText;
+                element.setAttribute("style", `${ordering[courseName]}`);
+            });
+        } else {
+            console.log("Parse default");
+            for (let i=0; i<items.length; i++) {
+                items[i].setAttribute("style", `order:${i+1};`);
+            }
         }
+        let bottomButton = document.getElementsByClassName("paging paging-morelink")[0];
+        bottomButton.setAttribute("style", `order:${items.length+1};`);
     }
-    let bottomButton = document.getElementsByClassName("paging paging-morelink")[0];
-    bottomButton.setAttribute("style", `order:${items.length+1};`);
 }
 
 function saveOrdering() {
@@ -162,5 +192,22 @@ function saveOrdering() {
 }
 
 function searchCourse(courseName) {
-    console.log(courseName);
+    let courses = document.querySelector(".courses");
+    let elements = courses.getElementsByClassName("coursebox");
+    let searched = document.getElementById("searched");
+    if (courseName !== "") {
+        elements.forEach((element) => {
+            let name = element.getElementsByClassName("info")[0].innerText.toLowerCase();
+            if (name.includes(courseName.toLowerCase())) {
+                searched.innerHTML = "";
+                let copy = element.cloneNode(true);
+                copy.setAttribute("style", copy.getAttribute("style") + " margin-bottom:0px;")
+                searched.appendChild(copy);
+                searched.setAttribute("class", "searchedfound");
+            }
+        })
+    } else {
+        searched.innerHTML = "";
+        searched.setAttribute("class", "searchedempty");
+    }
 }
