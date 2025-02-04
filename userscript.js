e581a9423aa9e09e83707852f29320776ac640dc
AINDUSTRIES <contact.aindustries@gmail.com>
2025-02-04 11:37:21 +0100

    Animation delay fix

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Moodle
 // @namespace    http://tampermonkey.net/
-// @version      2024-02-29
+// @version      2025-02-04
 // @description  Better, Stronger
 // @author       AINDUSTRIES
 // @match        https://moodle.umons.ac.be/*
@@ -13,17 +13,7 @@
 
 (function () {
     "use strict";
-    animate();
-    login();
-    let ordering = getOrdering();
-    parseStyle(ordering);
-    addUi(ordering);
-    addStyleSheet();
-})();
-
-function addStyleSheet() {
-    let stylesheet = document.createElement("style");
-    stylesheet.innerText = `
+    const styleSheet = `
     .navbar.fixed-top {
         background: linear-gradient(90deg in hsl shorter hue, #a80039 20%, rgb(0,0,150) 100%) !important;
         background-size: 500% 100% !important;
@@ -82,44 +72,57 @@ function addStyleSheet() {
         margin-bottom: 4px;
         padding: 2px 2px 2px 2px;
     }`;
-    document.head.appendChild(stylesheet);
+    animate();
+    login();
+    let ordering = getOrdering();
+    parseStyle(ordering);
+    addUi(ordering);
+    addStyleSheet(stylesheet);
+})();
+
+function addStyleSheet(styleSheet) {
+    let styleHeaderTag = document.createElement("style");
+    styleHeaderTag.innerText = styleSheet;
+    document.head.appendChild(styleHeaderTag);
 }
 
 function addUi() {
-    let courses = document.querySelector(".courses");
-    if (courses !== null) {
-        const div = document.getElementById("carousel-item-main");
+    const topRightMenu = document.getElementById("carousel-item-main");
 
-        let save = document.createElement("button");
-        save.textContent = "BetterMoodle Save Config";
-        save.addEventListener("click", saveOrdering);
-        save.setAttribute("class", "dropdown-item");
+    let saveConfigButton = document.createElement("button");
+    save.textContent = "BetterMoodle Save Config";
+    save.addEventListener("click", saveOrdering);
+    saev.className = "dropdown-item";
 
-        let del = document.createElement("button");
-        del.textContent = "BetterMoodle Delete Config";
-        del.addEventListener("click", deleteOrdering);
-        del.setAttribute("class", "dropdown-item");
+    let deleteConfigButton = document.createElement("button");
+    del.textContent = "BetterMoodle Delete Config";
+    del.addEventListener("click", deleteOrdering);
+    del.className = "dropdown-item";
 
-        div.insertBefore(save, div.getElementsByClassName("dropdown-divider")[1]);
-        div.insertBefore(del, div.getElementsByClassName("dropdown-divider")[1]);
+    let menuElementDivider = topRightMenu.getElementsByClassName("dropdown-divider")[1];
 
-        courses.getElementsByClassName("coursebox").forEach((element) => {
+    div.insertBefore(save, menuElementDivider);
+    div.insertBefore(del, menuElementDivider);
+
+    let courseOuterBox = document.querySelector(".courses");
+
+    if (courseOuterBox !== null) {
+        courseOuterBox.getElementsByClassName("coursebox")
+        .forEach((element) => {
             let index = parseInt(
-                element.getAttribute("style").split(":")[1].replace(";", ""),
+                course.style.order,
             );
-            let input = document.createElement("input");
-            let box = document.createElement("div");
-            box.setAttribute("class", "betterbox");
-            input.setAttribute("type", "number");
-            input.setAttribute("class", "input");
-            input.value = index;
-            box.appendChild(input);
-            element.appendChild(box);
-            input.addEventListener("change", () => {
-                let index = parseInt(
-                    element.getAttribute("style").split(":")[1].replace(";", ""),
-                );
-                moveCourse(index, input.value);
+            let courseNumberInput = document.createElement("input");
+            let inputContainer = document.createElement("div");
+            inputContainer.setAttribute("class", "betterbox");
+            courseNumberInput.type = "number";
+            courseNumberInput.className = "input";
+            courseNumberInput.value = index;
+            inputContainer.appendChild(courseNumberInput);
+            course.appendChild(box);
+            courseNumberInput.addEventListener("change", () => {
+                let index = parseInt(course.order);
+                moveCourse(index, courseNumberInput.value);
             });
         });
 
@@ -128,32 +131,38 @@ function addUi() {
         search.setAttribute("type", "text");
         search.setAttribute("class", "search");
         search.setAttribute("placeholder", "Recherchez un cours.");
-        search.addEventListener("input", () => {
-            searchCourse(search.value);
-        });
-        list.insertBefore(search, courses);
+        search.addEventListener("input", () => {searchCourse(search.value);});
+        list.insertBefore(search, courseOuterBox);
 
-        let searched = document.createElement("div");
-        searched.setAttribute("id", "searched");
-        list.insertBefore(searched, courses);
+        let searchedDiv = document.createElement("div");
+        searchedDiv.setAttribute("id", "searched");
+        list.insertBefore(searchedDiv, courseOuterBox);
     }
 }
 
 function animate() {
     let banner = document.getElementsByClassName("navbar")[0];
-    banner.style.setProperty("background-position", `0% 50%`, "important");
-    setInterval(() => {
-        let d = new Date();
-        let s = d.getSeconds();
-        let n = 0;
-        let p = parseInt(banner.style.backgroundPosition.replace("%", "").split(" ")[0]);
-        if (s <= 30) {
-            n = Math.round((s/30)*100);
-        } else if (s > 30) {
-            n = 100 - Math.round(((s-30)/30)*100);
+    let watcher = function(elem) {
+        if (elem === null) {
+            setTimeout(watcher, 100);
+        } else {
+            setInterval((elem) => {_animate(elem)}, 500);
         }
-        banner.style.setProperty("background-position", `${n}% 50%`, "important");
-    }, 1000);
+    }
+    watcher(banner);
+}
+
+function _animate(banner) {
+    let d = new Date();
+    let s = d.getSeconds();
+    let n = 0;
+    let p = parseInt(banner.style.backgroundPosition.replace("%", "").split(" ")[0]);
+    if (s <= 30) {
+        n = Math.round((s/30)*100);
+    } else if (s > 30) {
+        n = 100 - Math.round(((s-30)/30)*100);
+    }
+    banner.style.setProperty("background-position", `${n}% 50%`, "important");
 }
 
 function currentOrdering() {
